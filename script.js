7ae8bc42f174b8aa95c3e09d9746138474d31206
MiguelCarachenskiNeto <116725585+MiguelCarachenskiNeto@users.noreply.github.com>
2024-10-07 10:42:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function somar(a,b){
+    let total = a + b;
+    console.log(total);
+}
+
+somar(2,10);
+
+let subtrair=(c,d)=>{
+    let total = c-d;
+    console.log(total);
+}
+
+subtrair(15,7)
+
+let numeros -[1,2,3,10];
+numeros.map((item)=>{
+    console.log(item);
+})
